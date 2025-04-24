740646c8178464a477d8f09d6deffd49bd8c09fb
Paul Barnes <paulba71@hotmail.com>
2025-04-24 17:37:42 +0100

    Initial Commit

---
+++ Forest Fest/Forest_FestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Forest_FestApp.swift
+//  Forest Fest
+//
+//  Created by Paul Barnes on 24/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Forest_FestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
